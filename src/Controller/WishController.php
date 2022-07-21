bbe87461b64aaf964e6a62f4564dc76c9d0a4674
Salim <laimeche160@gmail.com>
2022-07-21 16:42:40 +0200

    second commit (connecting and testing bdd )

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,18 +14,29 @@ class WishController extends AbstractController
     /**
      * @Route("/wish", name="wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig', [
+        $wish = $wishRepository->findAll();
+        dump($wish);
 
+        return $this->render('wish/list.html.twig', [
+                "wish" => $wish
         ]);
     }
 
     /**
      * @Route("/wish/detail", name="wish_detail")
      */
-    public function detail(){
+    public function detail(int $id, WishRepository $wishRepository){
 
-        return $this->render('wish/wish-detail.html.twig');
+        $wish = $wishRepository->find($id);
+
+        if(!$wish) {
+            throw $this-> createNotFoundException('This wish do not exists.');
+        }
+
+        return $this->render('wish/wish-detail.html.twig',[
+            "wish"=> $wish
+        ]);
     }
 }
